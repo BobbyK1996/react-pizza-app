5a77c7b525708375f6a4d13d3e698e29fbe20f59
BobbyK1996 <bobbykamal@hotmail.co.uk>
2024-04-21 23:15:48 +0100

    Adjusted design for larger screens

--- src/ui/LinkButton.jsx
+++ src/ui/LinkButton.jsx
@@ -4,7 +4,7 @@ function LinkButton({ children, to }) {
   const navigate = useNavigate();
 
   const className =
-    'text-sm text-blue-500 transition-all duration-1000 ease-in-out hover:text-blue-600 hover:underline';
+    'text-sm text-blue-500 transition-all duration-1000 ease-in-out hover:text-blue-600 hover:underline md:text-blue-400';
 
   if (to === '-1')
     return (
